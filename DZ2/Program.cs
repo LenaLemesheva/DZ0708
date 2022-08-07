1e7e9822f28fedb6de430363cabc9b50a57de767
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-07 18:59:58 +0800

    Task 2

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberB>max) max = numberB;
+if (numberC>max) max = numberC;
+Console.Write("max = ");
+Console.WriteLine($"{max}");
+
