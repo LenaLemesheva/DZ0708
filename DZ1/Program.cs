14faf50827d66b0f0758f982a66d81970e81af95
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-07 17:34:33 +0800

    Task 1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.Write($"{numberA} - большее, {numberB} - меньшее");
+}
+else
+{
+    Console.Write($"{numberB} - большее, {numberA} - меньшее");
+}
+
