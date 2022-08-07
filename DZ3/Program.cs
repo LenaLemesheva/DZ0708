40f05ec926e00f0aa82570d752df5e7a558435fa
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-07 19:29:31 +0800

    Task 3

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.Write($"{number} - да, четное");
+}
+else
+{
+    Console.Write($"{number} - нет, нечетное");
+}
