8be867a79c7e316eb1f4f0dc836aac8c859afe66
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-10 21:32:39 +0800

    Fix Task15

--- DZ_Task15/Program.cs
+++ DZ_Task15/Program.cs
@@ -3,32 +3,13 @@
 6 -> да, 7 -> да, 1 -> нет. */
 
 Console.Write("Введите число от 1 до 7: ");
-int day = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 
-switch (day)
+if (number >= 6)
 {
-    case 1:
-       Console.WriteLine ("Понедельник - рабочий день");
-       break;
-    case 2:
-       Console.WriteLine ("Вторник - рабочий день");
-       break;
-    case 3:
-       Console.WriteLine ("Среда - рабочий день");
-       break;
-    case 4:
-       Console.WriteLine ("Четверг - рабочий день");
-       break;
-    case 5:
-       Console.WriteLine ("Пятница - рабочий день");
-       break;
-    case 6:
-       Console.WriteLine ("Суббота - выходной день");
-       break;
-    case 7:
-       Console.WriteLine ("Воскресенье - выходной день");
-       break;
-    default:
-       Console.WriteLine ("Вы ввели число, не входящее в числа от 1 до 7");
-       break;         
+    Console.WriteLine($"да");
+}
+else
+{
+Console.WriteLine ($"нет");
 }
