d3071c3452affae774d3bae7c0d64c797eccc896
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-17 15:32:36 +0800

    СеминарTask26

---
+++ СеминарTask26/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход число и
+выдает количество цифр в числе.
+Например: 456 -> 3, 78 -> 2, 89126 -> 5. */
+
+Console.Write("Ведите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int DigitCount(int a)
+{
+    int count = 0;
+    while (a != 0)
+// 1= - не равно
+    {
+        count++;
+        a /= 10;
+//      a = a / 10;
+    }
+    return count;
+}
+
+Console.Write(DigitCount(num));
