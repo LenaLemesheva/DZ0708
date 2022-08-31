e876a8214d7c156c101e4425fc66851c3fbcf823
ElenaLemesheva <lenleolem@yandex.ru>
2022-08-31 15:38:41 +0800

    СеминарTask53

---
+++ DZ_TaskStar2/Program.cs
@@ -0,0 +1,97 @@
+﻿/* Написать программу для перевода римских чисел в десятичные арабские.
+III -> 3
+LVIII -> 58
+MCMXCIV -> 1994 */
+
+/* Console.Write("Введите арабское число = ");
+string N = Convert.ToString(Console.ReadLine());
+
+int[] Roman = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+string[] Arab = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+int ChangeNum(string s)
+{
+   int[] rom = new int[100]; int n; int res;
+    res = 0;
+    int i = 0;
+
+    if (i < s.Length + 1)
+    {
+        switch (s[i])
+        {
+            case 'I':
+                {
+                    res += 1;
+                    i += 1;
+                    if (i < s.Length + 1)
+                    {
+                        switch (s[i])
+                        {
+                            case 'I':
+                                {
+                                    res += 1;
+                                    i += 1;
+                                    if (i < s.Length + 1)
+                                    {
+                                        res += 1;
+                                    }
+                                    break;
+                                }
+                            case 'V':
+                                {
+                                    res = 4;
+                                    break;
+                                }
+                            case 'X':
+                                {
+                                    res = 9;
+                                    break;
+                                }
+                        }
+                    }
+                }
+            case 'V':
+                {
+                    res = 5;
+                    i += 1;
+                    if (i < s.Length + 1)
+                    {
+                        res = 6;
+                        i += 1;
+                        if (i < s.Length + 1)
+                        {
+                            res = 7;
+                            i += 1;
+                            if (i < s.Length + 1)
+                            {
+                                res = 8;
+                            }
+                        }
+
+                    }
+                    break;
+                }
+                default: Console.Write("Такого римского числа не существует");
+        }
+        //if (s[i] == 'I') rom[i] = 1;
+        //if (s[i] == 'V') rom[i] = 5;
+        //if (s[i] == 'X') rom[i] = 10;
+        //if (s[i] == 'L') rom[i] = 50;
+        //if (s[i] == 'C') rom[i] = 100;
+        //if (s[i] == 'D') rom[i] = 500;
+        //if (s[i] == 'M') rom[i] = 1000;
+    }
+    //res = rom[0];
+    {
+        //for (i = 1; i < s.Length; i++)
+        {
+            //res += rom[i];
+        }
+        return res;
+    }
+}
+//return res;
+Console.Write("Вы получили римское число = ");
+Console.Write(ChangeNum(N));
+Console.WriteLine();
+*/
---
+++ СеминарTask53/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая поменяет
+местами первую и последнюю строку массива. */
+
+Console.Write("Ввелите количество строк = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов = ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Ввелите начало диапазона = ");
+int k = int.Parse(Console.ReadLine());
+Console.Write("Введите конец диапазона = ");
+int l = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void ChangeMatrixRows(int[,] matrix)
+{
+    int temp;
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0,i] = matrix[matrix.GetLength(0)-1, i];
+        matrix[matrix.GetLength(0)-1, i] = temp;      
+    }
+}
+void PrintArr(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MyMatrix = GetArray(m, n, k, l);
+PrintArray(MyMatrix);
+Console.WriteLine();
+
+ChangeMatrixRows(MyMatrix);
+PrintArr(MyMatrix);
