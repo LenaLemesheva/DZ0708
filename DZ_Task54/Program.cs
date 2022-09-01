ecf6fe67bab58331610d344d950e76e7805ed521
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-01 12:11:02 +0800

    DZ_Task54

---
+++ DZ_Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.Write("Ввелите количество строк = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов = ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Ввелите начало диапазона = ");
+int k = int.Parse(Console.ReadLine());
+Console.Write("Введите конец диапазона = ");
+int l = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void RowsReduction(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int s = 0; s < arr.GetLength(1) - j - 1; s++)
+            {
+                if (arr[i, s] < arr[i, s + 1])
+                {
+                    int tmp = arr[i, s];
+                    arr[i, s] = arr[i, s + 1];
+                    arr[i, s + 1] = tmp;
+                }
+            }
+        }
+    }
+}
+
+int[,] myArray = GetArray(m, n, k, l);
+PrintArray(myArray);
+Console.WriteLine();
+
+RowsReduction(myArray);
+PrintArray(myArray);
