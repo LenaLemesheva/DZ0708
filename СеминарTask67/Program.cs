731898e41b86b12e56643ee1339edca25c89a264
ElenaLemesheva <lenleolem@yandex.ru>
2022-09-03 14:18:26 +0800

    СеминарTask67

---
+++ СеминарTask67/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая будет принимать на вход 
+число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+int SumNumber(int num)
+{
+    return num <= 0 ? 0 : num % 10 + SumNumber(num / 10); //если сумма меньше нуля, то возвращаем ноль
+}   // if (num <= 0) return 0;                            //если сумма больше нуля то вызываем рекурсию
+    //else return num % 10 + SumNumber(num / 10);         //со значением с сокращением на один разряд
+                                                      
+Console.Write("Число = ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма цифр: " + SumNumber(number));
